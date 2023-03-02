76f3ae5879017cb4ab48cd2326fea256beb627b1
jacob <jacob@MSI-DESKTOP76>
2023-03-01 19:07:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace genericAnimal
+{
+    class Program
+    {
+        class Animal<T>
+        {
+            public T data { get; set; }
+            public Animal(T data)
+            {
+                this.data = data;
+            }
+            public T getAnimal()
+            {
+                return data;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Animal <string> animalName = new Animal <string> ("Horse");
+            Animal <string> animalHabitat = new Animal <string> ("Grasslands");
+            Animal <Boolean> endangered = new Animal <Boolean> (true);
+            Animal <Boolean> extinct = new Animal <Boolean> (false);
+            Animal <int> averageLifespan = new Animal <int> (15);
+
+            Console.WriteLine(animalName.getAnimal());
+            Console.WriteLine(animalHabitat.getAnimal());
+            Console.WriteLine(endangered.getAnimal());
+            Console.WriteLine(extinct.getAnimal());
+            Console.WriteLine(averageLifespan.getAnimal());
+        }
+    }
+}
